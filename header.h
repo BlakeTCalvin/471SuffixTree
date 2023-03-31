1e9f6086c58121e29d2b34d8dae7291da3c88a01
bcalvin1 <bcalvin1@eecs.wsu.edu>
2023-03-31 08:35:17 -0700

    More implementation on findpath

--- header.h
+++ header.h
@@ -54,7 +54,8 @@ public:
         setSequence(fasta);
         
         this->leaves = SEQUENCE.length();
-        this->nextID = this->leaves + 1;
+        this->nextInternalID = this->leaves + 1;
+        this->nextLeafID = 1;
 
         initializeTree();
     }
@@ -75,20 +76,74 @@ public:
     void findPath(Node *u, int index) {
         Node *v = u->parent;
         string x = SEQUENCE.substr(index);
+        int flag = 0;
 
+        // if u has no children, add leaf node
         if(u->children.empty()) {
-            Node *newNode = new Node(nextLeafID, 0, nullptr, " ");
+            Node *newNode = new Node(nextLeafID, u->stringDepth+1, u, x);
             u->children.push_back(newNode);
+            nextLeafID++;
         }
-        for (Node *node : u->children) {
+        else { // children exist
+            for (Node *node : u->children) {
+                if (node->parentEdgeLabel[0] == x[0]) { // child branch starts with x[0]
+                    flag == 1; // update flag to know we found an edge we can reuse
 
+                    // loop through parentEdgeLabel and check for matches on x
+                    for (int i=1; i<node->parentEdgeLabel.length(); i++) {
+                        if (node->parentEdgeLabel[i] != x[i]) { // mismatch
+                            // break edge
+                            string edgePart1 = node->parentEdgeLabel.substr(0, i-1);
+                            string edgePart2 = node->parentEdgeLabel.substr(i);
+                            string newLeafEdge = x.substr(i);
+
+                            // remove old child from u
+                            for (auto it = u->children.begin(); it != u->children.end(); ++it) {
+                                if (*it == node) { // correct node is found in children vector
+                                    u->children.erase(it);
+                                    break;
+                                }
+                            }
+
+                            // new internal node
+                            Node *newInternalNode = new Node(nextInternalID, u->stringDepth+1, u, edgePart1);
+                            nextInternalID++;
+
+                            // make internal node new child
+                            u->children.push_back(newInternalNode);
+
+                            // update child node 
+                            node->parent = newInternalNode;
+                            node->stringDepth++;
+                            node->parentEdgeLabel = edgePart2;
+
+                            // new leaf node
+                            Node *newLeafNode = new Node(nextLeafID, node->stringDepth, newInternalNode, newLeafEdge);
+                            nextLeafID++;
+
+                            // adding both new nodes to internals children
+                            newInternalNode->children.push_back(node);
+                            newInternalNode->children.push_back(newLeafNode);
+                            return;
+                        }
+                    }
+
+                    // parentEdgeLabel was exhausted
+                    // TODO
+                }
+            }
+            if (flag == 0) { // no child has edge label start with x[0]. Insert new child under u
+                Node *newNode = new Node(nextLeafID, u->stringDepth+1, u, x);
+                u->children.push_back(newNode);
+                nextLeafID++;
+            }
         }
     }
 
     void initializeTree() { //TODO
         this->root = new Node(nextInternalID, 0, nullptr, " ");
         this->root->suffixLink = root;
-        this->nextInternalID++;
+        nextInternalID++;
     }
 
 private:
